84014cfd1a36f7060f574719d2fb6577d40fd5a4
single-byte <70197335+single-byte@users.noreply.github.com>
2023-07-18 23:45:28 +0500

    Added Custom Generic Types

--- ts-proj-setup/src/myGenerics.ts
+++ ts-proj-setup/src/myGenerics.ts
@@ -29,3 +29,51 @@ interface Bootle {
 
 identityFour<Bootle>({ brand: "Coca Cola", type: 2 });
 
+function getSearchProducts<T>(products: T[]): T {
+  const myIndex = 3
+  return products[myIndex];  //return should be one of the array elements
+}
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  // do some db operations
+  const myIndex = 4
+  return products[myIndex];  //return should be one of the array elements
+}
+
+interface Database {
+  connection: string,
+  username: string,
+  password: string
+}
+
+function anotherFunc<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne, valTwo
+  }
+}
+
+anotherFunc(3, { connection: "", username: "", password: "" });
+
+
+// Custom Generic Data Type
+interface Quiz {
+  name: string,
+  type: string,
+}
+
+interface Course {
+  name: string,
+  author: string,
+  subject: string,
+}
+
+class Sellable<T>{
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
+
+const sellable = new Sellable<Quiz>();
+sellable.addToCart({ name: "quiz1", type: "quiz" });
